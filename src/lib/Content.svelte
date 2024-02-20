2bd5239c50f1a598c9727cb28c1e91cb175f9610
Callum Laing <callum.laing91@hotmail.co.uk>
2024-02-20 21:52:07 +0000

    added base game using numbers rather than images for the cards

--- src/lib/Content.svelte
+++ src/lib/Content.svelte
@@ -1,9 +1,30 @@
+<script>
+let cards = [1, 2, 3, 4, 5];
+let previousNumber = null;
+
+let shuffleCards = () => {
+    for (let i = cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cards[i], cards[j]] = [cards[j], cards[i]];
+    }
+}
+
+let clickButton = (number) => {
+    
+    if (number === previousNumber) {
+        console.log("Game over!!!");
+    }
+    shuffleCards();
+    previousNumber = number;
+
+}
+
+</script>
+
 <div class="cardContainer">
-    <div class="card">1</div>
-    <div class="card">2</div>
-    <div class="card">3</div>
-    <div class="card">4</div>
-    <div class="card">5</div>
+    {#each cards as card (card)}
+    <button on:click={() => clickButton(card)} class="card">{card}</button>
+{/each}
 </div>
 
 <style>
@@ -20,5 +41,8 @@
         border: 2px solid rgba(150, 150, 250);
         margin: 20px;
         border-radius: 10px;
+        background-color: black;
+        color: white;
+        font-size: 4rem;
     }
 </style>
