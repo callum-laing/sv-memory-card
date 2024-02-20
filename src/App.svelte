97e38e733f1c71d009f8f13ee446ec086946b465
Callum Laing <callum.laing91@hotmail.co.uk>
2024-02-20 21:20:08 +0000

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Content from './lib/Content.svelte';
+</script>
+
+
+
+<h1>Memory Card</h1>
+<main>
+  <Content />
+
+</main>
+
+
+<style>
+
+</style>
---
+++ src/lib/Content.svelte
@@ -0,0 +1,24 @@
+<div class="cardContainer">
+    <div class="card">1</div>
+    <div class="card">2</div>
+    <div class="card">3</div>
+    <div class="card">4</div>
+    <div class="card">5</div>
+</div>
+
+<style>
+    .cardContainer {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .card {
+        text-align: center;
+        height: 300px;
+        width: 200px;
+        border: 2px solid rgba(150, 150, 250);
+        margin: 20px;
+        border-radius: 10px;
+    }
+</style>
